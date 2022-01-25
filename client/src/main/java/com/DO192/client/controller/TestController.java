8823dc551813c53a0f1046fbf7a3f3cdec272016
cah-nithin-c <97599635+cah-nithin-c@users.noreply.github.com>
2022-01-25 16:06:43 +0530

    working with /refresh

--- client/src/main/java/com/DO192/client/controller/TestController.java
+++ client/src/main/java/com/DO192/client/controller/TestController.java
@@ -1,17 +1,19 @@
 package com.DO192.client.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class TestController {
 
-//	@Value("${my.greeting}")
-//	private String getGreeting;
+	@Value("${my.greetings}")
+	private String getGreeting;
 	
 	@GetMapping("/greeting")
 	public String getGreet() {
-		return "";
+		return getGreeting;
 	}
 }
